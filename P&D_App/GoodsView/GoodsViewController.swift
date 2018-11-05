b0386dca57b44456a48620ea5199fbacc7cd2493
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-11-05 19:19:24 +0900

    add Files

---
+++ P&D_App/GoodsView/GoodsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoodsViewController.swift
+//  P&D_App
+//
+//  Created by Azuma on 2018/11/05.
+//  Copyright © 2018 P&D. All rights reserved.
+//
+
+import UIKit
+
+class GoodsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
