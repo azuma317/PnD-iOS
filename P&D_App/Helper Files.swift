28479f067fd285bf3b2d858dea8c40dd9d96cb0e
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-11-06 11:33:39 +0900

    login画面のアニメーション追加

---
+++ P&D_App/Helper Files.swift
@@ -0,0 +1,9 @@
+//
+//  Helper Files.swift
+//  P&D_App
+//
+//  Created by Azuma on 2018/11/06.
+//  Copyright © 2018 P&D. All rights reserved.
+//
+
+import UIKit
--- P&D_App/HomeView/HomeViewController.swift
+++ P&D_App/HomeView/HomeViewController.swift
@@ -7,13 +7,69 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class HomeViewController: UIViewController {
+
+    @IBOutlet private weak var logoImageView: UIImageView!
+    @IBOutlet private var loginView: UIView!
+    @IBOutlet private weak var logoTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var logoHorizontalConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var keyTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var warningLabel: UILabel!
+
+    private var isLogin: Bool = false
+    private var loginViewTopConstraint: NSLayoutConstraint!
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initView()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.view.layoutIfNeeded()
+    }
+
+    private func initView() {
+        bind()
+        customization()
+    }
+
+    private func bind() {
+        Observable.combineLatest(nameTextField.rx.text, keyTextField.rx.text)
+            .subscribe({ [unowned self] in
+                self.loginButton.isEnabled = ($0.element?.0?.isNotEmpty)! && ($0.element?.1?.isNotEmpty)!
+            })
+            .disposed(by: disposeBag)
+
+        loginButton.rx.tap
+            .subscribe({ _ in
+                // loginの処理
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func customization() {
+        logoTopConstraint.priority = UILayoutPriority(rawValue: 1000)
+        logoHorizontalConstraint.priority = UILayoutPriority(rawValue: 999)
+        UIView.animate(withDuration: 1.5, delay: 0.5, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: { _ in
+            self.view.insertSubview(self.loginView, belowSubview: self.logoImageView)
+            self.loginView.translatesAutoresizingMaskIntoConstraints = false
+            self.loginView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.loginViewTopConstraint = NSLayoutConstraint.init(item: self.loginView, attribute: .top, relatedBy: .equal, toItem: self.logoImageView, attribute: .bottom, multiplier: 1, constant: 40)
+            self.loginViewTopConstraint.isActive = true
+            self.loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.45).isActive = true
+            self.loginView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+            self.view.layoutIfNeeded()
+        })
     }
     
 
